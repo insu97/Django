795fcee6aa3289d97ff073237b2b9a8e715ac568
insu97 <52162653+insu97@users.noreply.github.com>
2023-12-20 16:31:50 +0900

    new

--- config/views.py
+++ config/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def main(request):
-    return HttpResponse("안녕하세요.")
+    return render(request, "main.html")
 
 def burger_list(request):
-    return HttpResponse("햄버거 목록입니다.")
+    return render(request, "burger_list.html")
