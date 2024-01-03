575db1acaebdf07f0059093f4cad7cbbea5b241a
insu97 <52162653+insu97@users.noreply.github.com>
2024-01-03 15:37:27 +0900

    2024-01-03

--- config/views.py
+++ config/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from burgers.models import Burger
 
 def main(request):
     return render(request, "main.html")
 
 def burger_list(request):
-    return render(request, "burger_list.html")
+    burgers = Burger.objects.all()
+    print("전체 햄버거 목록 : ", burgers)
+
+    context = {
+        "burgers": burgers, # burgers 키에 burgers 변수(QuerySet 객체)를 전달
+    }
+
+    return render(request, "burger_list.html", context)
